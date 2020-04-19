c4d6847be0022609bc2b6afed5a06dc18f194dcc
dm6593120 <dm6593120@gmail.com>
2020-04-20 02:52:51 +0530

    Add files via upload

---
+++ twosumleetcode.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class twosumleetcode{
+	public static void main(String[] args){
+		
+		int[] ar ={3,7,11,2};
+		int[] array=find(ar,9);
+		for(int a:array)
+		{
+			System.out.println(a);
+		}
+	
+	
+	}
+	public static int[] find(int[] nums,int target){
+			int[] array= new int[2];
+		for(int i=0;i<nums.length;i++)
+       	 {
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]+nums[j]==target)
+                {
+                    array[0]=i;
+                    array[1]=j;
+                    return array;
+                }
+    
+        	}
+    	}
+    	return array;
+    }
+	}
